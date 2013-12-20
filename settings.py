2b5c7e543b8fd66d11df5906bdfe6eac705758c4
王健 <wangjian2254@gmail.com>
2013-12-20 11:55:54 +0800

    rds 数据库配置

--- settings.py
+++ settings.py
@@ -1,7 +1,6 @@
 # Django settings for zt project.
 import logging
 import os
-import os
 os.environ['LANG'] = 'en_US.UTF-8'
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
@@ -21,22 +20,22 @@ DATABASES = {
 #        'HOST': '192.168.101.73',                      # Set to empty string for localhost. Not used with sqlite3.
 #        'PORT': '1433',                      # Set to empty string for default. Not used with sqlite3.
 #    }
-#     'default': {
-#         'ENGINE': 'mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'zt20130120',                      # Or path to database file if using sqlite3.
-#         'USER': 'root',                      # Not used with sqlite3.
-#         'PASSWORD': '123456',                  # Not used with sqlite3.
-#         'HOST': '69.16.97.123',                      # Set to empty string for localhost. Not used with sqlite3.
-#         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
-#     }
-   'default': {
-           'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-           'NAME': 'zt.db',                      # Or path to database file if using sqlite3.
-           'USER': 'root',                      # Not used with sqlite3.
-           'PASSWORD': '123456',                  # Not used with sqlite3.
-           'HOST': '69.16.97.123',                      # Set to empty string for localhost. Not used with sqlite3.
-           'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
-       }
+     'default': {
+         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+         'NAME': 'zt2',                      # Or path to database file if using sqlite3.
+         'USER': 'wangjian2254',                      # Not used with sqlite3.
+         'PASSWORD': '05992254wj',                  # Not used with sqlite3.
+         'HOST': 'testdbinstance.cgkvyl8jtz2d.ap-northeast-1.rds.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
+         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+     }
+   #'default': {
+   #        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+   #        'NAME': 'zt.db',                      # Or path to database file if using sqlite3.
+   #        'USER': 'root',                      # Not used with sqlite3.
+   #        'PASSWORD': '123456',                  # Not used with sqlite3.
+   #        'HOST': '69.16.97.123',                      # Set to empty string for localhost. Not used with sqlite3.
+   #        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+   #    }
 }
 
 # Local time zone for this installation. Choices can be found here:
